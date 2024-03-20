e16373030f25272549675fb0aae55b0149941929
caje777 <160929942+caje777@users.noreply.github.com>
2024-03-20 10:04:22 -0600

    Update and rename app.py to text_analyzer.py

--- text_analyzer.py
+++ text_analyzer.py
@@ -1,15 +1,4 @@
-from flask import Flask, render_template, request
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-@app.route('/analyze', methods=['POST'])
-def analyze():
-    text = request.form['text']
-
+def analyze_text(text):
     number_of_vowels = 0
     number_of_consonants = 0
     number_of_y = 0
@@ -26,12 +15,17 @@ def analyze():
         elif letter == ' ':
             number_of_spaces += 1
         elif letter in '!.?':
-            number_of_sentences += 1
+            number_of_sentences +=1
 
     number_of_letters = number_of_vowels + number_of_consonants + number_of_y
     number_of_words = len(text.split())
 
-    return render_template('result.html', vowels=number_of_vowels, consonants=number_of_consonants, letters=number_of_letters, y=number_of_y, spaces=number_of_spaces, words=number_of_words, sentences=number_of_sentences)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    return {
+        'vowels': number_of_vowels,
+        'consonants': number_of_consonants,
+        'total_letters': number_of_letters,
+        'y': number_of_y,
+        'spaces': number_of_spaces,
+        'words': number_of_words,
+        'sentences': number_of_sentences
+    }
